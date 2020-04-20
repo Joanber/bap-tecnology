51aa1b9be3d9882bf9ed8159aa22c9f8c813203a
MrRainx <diegocondo10@yahoo.es>
2020-04-20 00:35:25 -0500

    registro de usuarios

--- src/app/models/cliente/cliente.ts
+++ src/app/models/cliente/cliente.ts
@@ -1,16 +1,16 @@
-import { Servicio } from '../servicio/servicio';
+import { Servicio } from "../servicio/servicio";
 
 export class Cliente {
-    identificacion?:string;
-    primer_nombre?:string;
-    segundo_nombre? :string;
-    primer_apellido?:string;
-    segundo_apellido?:string;
-    fecha_nacimiento?:string;
-    correo?:string;
-    telefono?:string;
-    direccion?:string;
-    username?:string;
-    password?:string;
-    servicio?:Servicio;
+  identificacion?: string;
+  primer_nombre?: string;
+  segundo_nombre?: string;
+  primer_apellido?: string;
+  segundo_apellido?: string;
+  fecha_nacimeinto?: Date;
+  correo?: string;
+  telefono?: string;
+  direccion?: string;
+  username?: string;
+  password?: string;
+  servicio?: Servicio;
 }
